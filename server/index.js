659e13076dbd90414f82c1d5d0b789f7992ea09c
Valdemar Černiavski <32278772+valdemarcz@users.noreply.github.com>
2022-10-23 16:06:42 +0300

    Functionality added

--- server/index.js
+++ server/index.js
@@ -38,45 +38,47 @@ const db = mysql.createPool({
 });
 
  
-const verifyJWT = (req, res, next) => {
+const verifyJWT = (role) => {
+    return (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const authorized = false;
-        const decoded = jwt.verify(
-          token,
-          'SECRETKEY'
-        );
+        const token = req.headers.authorization.split(' ')[1];  
+        let authorized = false;
+        const decoded = jwt.verify(token, 'SECRETKEY' );
+        
         req.userData = decoded;
+        
+        if(req.userData.role == role)
+            authorized = true;
 
-        const sqlGet = "SELECT role FROM User where id = ? and role = ?";
-
-        db.query(sqlGet,decoded.userId, role, (error, result) => {
-            if(error) {
-                console.log(error);
-            }
-            authorized=true;
-        })    
-
-        if (authorized) {
-            return res.status(403).send({
-                msg: 'Endpoint is only accessible by: Admin'
-            });
-        }
-
+        if (authorized == false)
+            throw Error("Unauthorized");
+        
         next();
+
       } catch (err) {
+          console.log(err.message);
+          let msg = "";
+
+          if(err.message == "Unauthorized"){
+            msg = "User needs to have role: " + role + " to access this endpoint";
+          } else {
+           msg = "Your token is incorrect or have expired";
+          }
         return res.status(401).send({
-          msg: 'Your session is not valid!'
+          msg: msg
         });
       }
+    }
 };
 
-const Role = require('./role.js');
+const role = require('./role.js');
 
-app.get("/api/getAttenders", verifyJWT.role="Admin", ( req, res) => {
-    const sqlGet = "SELECT * FROM Attender";
+app.get("/api/getAttenders/:id",  ( req, res) => {
+    const { id } = req.params;
 
-    db.query(sqlGet, (error, result) => {
+    const sqlGet = "SELECT * FROM Attender WHERE organized_by = ?";
+
+    db.query(sqlGet,id, (error, result) => {
         res.send(result);
     })
 })
@@ -108,10 +110,10 @@ app.put("/api/update/:id", (req, res) => {
 })
 
 app.post("/api/addAttender", (req, res) => {
-    const { namesurname, email, age } = req.body;
+    const { namesurname, email, age, userId } = req.body;
     const sqlInsert = 
-    "INSERT INTO Attender (namesurname, email, age) VALUES (?, ?, ?)";
-    db.query(sqlInsert, [namesurname, email, age], (error, result) => {
+    "INSERT INTO Attender (namesurname, email, age, organized_by) VALUES (?, ?, ?, ?)";
+    db.query(sqlInsert, [namesurname, email, age, userId], (error, result) => {
         if (error) {
             console.log(error);
         }
@@ -155,7 +157,7 @@ const validateRegister = (req, res, next) => {
 }
 
 
-app.post('/api/register', validateRegister, (req, res, next)=> {
+app.post('/api/register',  (req, res, next)=> {
     db.query(
         `SELECT * FROM User WHERE LOWER(username) = LOWER(${db.escape(
           req.body.username
@@ -175,9 +177,9 @@ app.post('/api/register', validateRegister, (req, res, next)=> {
               } else {
                 // has hashed pw => add to database
                 db.query(
-                  `INSERT INTO User (username, password, registered) VALUES ( ${db.escape(
+                  `INSERT INTO User (username, password, role, registered) VALUES ( ${db.escape(
                     req.body.username
-                  )}, ${db.escape(hash)}, now())`,
+                  )}, ${db.escape(hash)}, ${db.escape(req.body.role)}, now())`,
                   (err, result) => {
                     if (err) {
                       throw err;
@@ -197,7 +199,7 @@ app.post('/api/register', validateRegister, (req, res, next)=> {
       );
 });
 
-app.get('/isUserAuth', verifyJWT , (req, res) => {
+app.get('/isUserAuth', (req, res) => {
     res.send("You are authenticated Congrats:")
 })
 
@@ -224,6 +226,9 @@ app.post('/login', (req, res) => {
             (bErr, bResult) => {
               // wrong password
               if (bErr) {
+                  console.log(req.body.password,
+                    result[0]['password']);
+                  console.log(bErr);
                 throw bErr;
                 return res.status(401).send({
                   msg: 'Username or password is incorrect!'
@@ -232,7 +237,8 @@ app.post('/login', (req, res) => {
               if (bResult) {
                 const token = jwt.sign({
                     username: result[0].username,
-                    userId: result[0].id
+                    userId: result[0].id,
+                    role: result[0].role,
                   },
                   'SECRETKEY', {
                     expiresIn: '7d'
@@ -255,6 +261,94 @@ app.post('/login', (req, res) => {
         }
       );
 });
+
+
+app.get("/api/getUsers", ( req, res) => {
+    const sqlGet = "SELECT * FROM User";
+
+    db.query(sqlGet, (error, result) => {
+        res.send(result);
+    })
+})
+
+app.get("/api/getUser/:id", (req, res) => {
+    const { id } = req.params;
+
+    const sqlGet = "SELECT * FROM User WHERE id = ?";
+
+    db.query(sqlGet,id, (error, result) => {
+        if(error) {
+            console.log(error);
+        }
+        res.send(result);
+    })
+})
+
+app.put("/api/updateUser/:id", (req, res) => {
+    const { id } = req.params;
+    const { username, password, role } = req.body;
+   
+    db.query(
+        `SELECT * FROM User WHERE LOWER(username) = LOWER(${db.escape(
+          req.body.username
+        )});`,
+        (err, result) => {
+            console.log(result[0].id == req.params.id);
+          if ((result.length>0) && !(result[0].id !== req.params.id)) {
+            return res.status(409).send({
+              msg: 'This username is already in use!'
+            });
+          } else {
+            // username is available
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+              if (err) {
+                console.log(err);
+                return res.status(500).send({
+                  msg: err
+                });
+                
+              } else {
+                // has hashed pw => add to database
+                db.query(
+                  `UPDATE User set username="${req.body.username}", password=${db.escape(hash)}, role="${req.body.role}" WHERE id=${id}`,
+                  (err, result) => {
+                    if (err) {
+                      throw err;
+                      return res.status(400).send({
+                        msg: err
+                      });
+                    }
+                    return res.status(201).send({
+                      msg: 'User have been updated!'
+                    });
+                  }
+                );
+              }
+            });
+          }
+        }
+      );
+})
+
+
+app.delete("/api/removeUser/:id", (req, res) => {
+    const { id } = req.params;
+    const sqlDelete = 
+    "DELETE FROM User where id = ?";
+    db.query(sqlDelete, id, (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+});
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
     /*const sqlInsert = "INSERT INTO Attender (namesurname, email, age) VALUES ('asldkfa', 'alskjdfkjas@aaa.aaa', 22)";
